77b337b5f239efbb6a0fa98d319b7a09aaa4072a
François Schmidts <francois@schmidts.fr>
2018-11-08 11:08:58 +0100

    initial commit

---
+++ tests/redis_test.py
@@ -0,0 +1,50 @@
+import json
+import unittest
+from redis import Redis
+from prometheus_distributed_client import Metric
+from prometheus_distributed_client.metrics_registry import (add_metric_backend,
+        _all_metrics_defs, _all_metrics_backends)
+from prometheus_distributed_client.metric_backends import (
+        PrometheusPullBackend, PrometheusPushBackend, FlaskUtils)
+
+
+class PDCTestCase(unittest.TestCase):
+
+    @staticmethod
+    def _get_redis_creds():
+        with open('.redis.json') as fd:
+            return json.load(fd)
+
+    def setUp(self):
+        add_metric_backend('PrometheusPullBackend', PrometheusPullBackend())
+
+        add_metric_backend('PrometheusPushBackend',
+                PrometheusPushBackend(**self._get_redis_creds()))
+
+    def tearDown(self):
+        redis = Redis(**self._get_redis_creds())
+        for metric in _all_metrics_defs.values():
+            redis.delete(metric.name)
+        _all_metrics_defs.clear()
+        _all_metrics_backends.clear()
+
+    def test_redis(self):
+        metric1 = Metric('shruberry', 'shruberry', 'COUNTER')
+        metric2 = Metric('fleshwound', 'fleshwound', 'COUNTER', {'cross': {}})
+        metric1.inc()
+        metric2.inc()
+        metric2.inc(labels={'cross': 'eki'})
+        metric2.inc(labels={'cross': 'eki'})
+        metric2.inc(labels={'cross': 'patang'})
+        self.assertEqual(
+"""# HELP shruberry_total shruberry
+# TYPE shruberry_total counter
+shruberry_total 1.0
+""", FlaskUtils.flask_to_prometheus([metric1]))
+        self.assertEqual(
+"""# HELP fleshwound_total fleshwound
+# TYPE fleshwound_total counter
+fleshwound_total{cross=""} 1.0
+fleshwound_total{cross="eki"} 2.0
+fleshwound_total{cross="patang"} 1.0
+""", FlaskUtils.flask_to_prometheus([metric2]))
